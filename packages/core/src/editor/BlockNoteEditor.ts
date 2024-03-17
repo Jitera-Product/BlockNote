29e3709bf8ecccfb14aeb484079435afe5521b97
viettran1010 <viettran10101988@gmail.com>
2024-03-18 06:53:08 +0700

    fix: remove style warning

--- packages/core/src/editor/BlockNoteEditor.ts
+++ packages/core/src/editor/BlockNoteEditor.ts
@@ -1022,7 +1022,10 @@ export class BlockNoteEditor<
     for (const mark of marks) {
       const config = this.styleSchema[mark.type.name];
       if (!config) {
-        console.warn("mark not found in styleschema", mark.type.name);
+        if (mark.type.name !== "customContentProps") {
+          // workaround, ignore customContentProps
+          console.warn("mark not found in styleschema", mark.type.name);
+        }
         continue;
       }
       if (config.propSchema === "boolean") {
